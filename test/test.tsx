2a540f7fc52a2ddb138ea17325172a4fa2a6984f
skt-t1-byungi <tiniwz@gmail.com>
2019-09-07 18:01:29 +0900

    test: file rename, add rerender test

--- test/test.tsx
+++ test/test.tsx
@@ -1,4 +1,5 @@
 import test from 'ava'
+import * as React from 'react'
 import { renderHook, act } from '@testing-library/react-hooks'
 import InnoTrans = require('inno-trans')
 import InnoTransReactPlugin = require('../src/index')
@@ -16,6 +17,7 @@ test('change locale', t => {
     })
 
     const { result } = renderHook(() => trans.useT().t('aa'))
+
     t.is(result.current, 'ddd')
     act(() => { trans.locale('b') })
     t.is(result.current, 'zzz')
@@ -32,9 +34,30 @@ test('add filter', t => {
     })
 
     const { result } = renderHook(() => trans.useT().t('aa', { 0: 'z' }))
+
     t.is(result.current, 'aaaz')
     act(() => { trans.addFilter('b', v => `~${v}~`) })
     t.is(result.current, 'aaa~z~')
     act(() => { trans.addFilter('a', v => `%${v}%`) })
     t.is(result.current, 'aaa~%z%~')
+})
+
+test.only('child are not rerendered more than once,', t => {
+    const trans = InnoTrans({ locale: 'a' })
+    let calls = 0
+
+    renderHook(() => {
+        trans.useT()
+        calls++
+    }, {
+        wrapper: ({ children }) => {
+            trans.useT()
+            calls++
+            return <div>{children}</div>
+        }
+    })
+
+    t.is(calls, 2)
+    act(() => { trans.locale('b') })
+    t.is(calls, 4)
 })
